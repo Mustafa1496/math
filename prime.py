2d34a466f2c70f7c468332fed3e314cfa74f26c8
Mustafa_Cakir <cakirmustafa018@gmail.com>
2023-06-15 18:59:52 +0300

    legendre_symbol func added

--- prime.py
+++ prime.py
@@ -17,8 +17,15 @@ class Prime:
                 primes.append(num)
         return primes
         
+    def legendre_symbol(self, a, p):
+            if not self.check_prime(p) or p == 2:
+                raise ValueError("p must be an odd prime")
+            ls = pow(a, (p-1)//2, p)
+            return -1 if ls == p-1 else ls
+    
 num = Prime()
 print(num.check_prime(19)) 
 print(num.find_primes(10,50))
+print(num.legendre_symbol(2,23))
 
     
