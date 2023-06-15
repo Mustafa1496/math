970e5789de1f30b9efdba1b34c2b79f740facc13
Mustafa_Cakir <cakirmustafa018@gmail.com>
2023-06-15 18:51:59 +0300

    fixed for edge cases

--- prime.py
+++ prime.py
@@ -3,14 +3,14 @@ class Prime:
         pass
 
     def check_prime(self,num):
-        self.num = num
-        for i in range(2,int(num**0.5)):
-            if num%i == 0:
-                return i
-        else:
-            return num
+        if num < 2: 
+            return False
+        for i in range(2, int(num**0.5) + 1):
+            if num % i == 0:
+                return False
+        return True
         
 num = Prime()
-print(num.check_prime(19))
+print(num.check_prime(19)) 
 
     
