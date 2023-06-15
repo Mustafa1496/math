0f539fe591293d0a03eb1b8ff8aec1c041e62375
Mustafa_Cakir <cakirmustafa018@gmail.com>
2023-06-15 18:53:49 +0300

    find_primes func added

--- prime.py
+++ prime.py
@@ -9,8 +9,16 @@ class Prime:
             if num % i == 0:
                 return False
         return True
+    
+    def find_primes(self, start, end):
+        primes = []
+        for num in range(start, end + 1):
+            if self.check_prime(num):
+                primes.append(num)
+        return primes
         
 num = Prime()
 print(num.check_prime(19)) 
+print(num.find_primes(10,50))
 
     
